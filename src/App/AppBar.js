0d5414c1aa36684d01c1cbedfc33140d07ac1157
Ezekiel De Leon <ezekiel.de.leon@jci.com>
2019-05-24 09:58:26 -0700

    Approvider hooked up with App bar.

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import { AppContext } from './AppProvider';
 
 const Logo = styled.div`font-size: 1.5em;`;
 
@@ -14,7 +15,15 @@ const ControlButtonElem = styled.div`
 `;
 
 function ControlButton({ name, active }) {
-	return <ControlButtonElem active={active}>{toProperCase(name)}</ControlButtonElem>;
+	return (
+		<AppContext.Consumer>
+			{({ page, setPage }) => (
+				<ControlButtonElem active={page === name} onClick={() => setPage(name)}>
+					{toProperCase(name)}
+				</ControlButtonElem>
+			)}
+		</AppContext.Consumer>
+	);
 }
 
 function toProperCase(lower) {
