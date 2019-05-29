5b5ba8c0f9e6d86ee99aac332bd790f83db8930a
Ezekiel De Leon <ezekiel.de.leon@jci.com>
2019-05-29 09:46:01 -0700

    Add Coin Grid.

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import ConfirmButton from './ConfirmedButton';
-import Page from '../Shared/Page'
+import Page from '../Shared/Page';
+import CoinGrid from './CoinGrid';
 
 export default function() {
 	return (
 		<Page name="settings">
 			<WelcomeMessage />
 			<ConfirmButton />
+			<CoinGrid />
 		</Page>
 	);
 }
