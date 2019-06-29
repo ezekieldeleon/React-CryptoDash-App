c3dd45e2f111ae9266f48cb4d3e2d987dd064b40
Ezekiel De Leon <ezekiel.d.e@ieee.org>
2019-06-28 21:30:21 -0700

    save updated spaces.

--- src/Dashboard/priceTile.js
+++ src/Dashboard/priceTile.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
-import { SelectableTile } from '../Shared/Tile'
-import { fontSize3, fontSizeBig } from '../Shared/Styles';
-import { CoinHeaderGridStyled } from '../Settings/CoinHeaderGrid';
+import React from "react";
+import styled, { css } from "styled-components";
+import { SelectableTile } from "../Shared/Tile";
+import { fontSize3, fontSizeBig } from "../Shared/Styles";
+import { CoinHeaderGridStyled } from "../Settings/CoinHeaderGrid";
 
 const JustifyRight = styled.div`
   justify-self: right;
@@ -18,64 +18,64 @@ const TickerPrice = styled.div`
 
 const ChangePct = styled.div`
   color: green;
-  ${props => props.red && css`color: red;`}
+  ${props =>
+    props.red &&
+    css`
+      color: red;
+    `}
 `;
 
 const numberFormat = number => {
-  return +(number + '').slice(0, 7);
-}
+  return +(number + "").slice(0, 7);
+};
 
 const PriceTileStyled = styled(SelectableTile)`
-  ${props => props.compact && css`
-    display: grid;
-    ${fontSize3};
-    grid-gap: 5px;
-    grid-template-columns: repeat(3, 1fr);
-    justify-items: right;
-  `}
+  ${props =>
+    props.compact &&
+    css`
+      display: grid;
+      ${fontSize3};
+      grid-gap: 5px;
+      grid-template-columns: repeat(3, 1fr);
+      justify-items: right;
+    `}
 `;
 
-function ChangePercent({data}) {
+function ChangePercent({ data }) {
   return (
     <JustifyRight>
       <ChangePct red={data.CHANGEPCT24HOUR < 0}>
         {numberFormat(data.CHANGEPCT24HOUR)}
       </ChangePct>
     </JustifyRight>
-  )
+  );
 }
 
-function PriceTile({sym, data}) {
+function PriceTile({ sym, data }) {
   return (
     <PriceTileStyled>
       <CoinHeaderGridStyled>
         <div> {sym} </div>
         <ChangePercent data={data} />
       </CoinHeaderGridStyled>
-      <TickerPrice>
-        ${numberFormat(data.PRICE)}
-      </TickerPrice>
+      <TickerPrice>${numberFormat(data.PRICE)}</TickerPrice>
     </PriceTileStyled>
-  )
+  );
 }
 
-function PriceTileCompact({sym, data}){
+function PriceTileCompact({ sym, data }) {
   return (
     <PriceTileStyled compact>
       <JustifyLeft> {sym} </JustifyLeft>
       <ChangePercent data={data} />
-      <div>
-        ${numberFormat(data.PRICE)}
-      </div>
+      <div>${numberFormat(data.PRICE)}</div>
     </PriceTileStyled>
-  )
+  );
 }
 
-export default function({price, index}) {
+export default function({ price, index }) {
   let sym = Object.keys(price)[0];
-  let data = price[sym]['USD'];
+  let data = price[sym]["USD"];
   let TileClass = index < 5 ? PriceTile : PriceTileCompact;
-  return (
-    <TileClass sym={sym} data={data}>
-    </TileClass>)
+  return <TileClass sym={sym} data={data} />;
 }
