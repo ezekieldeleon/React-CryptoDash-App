ef9aef13c9fd05903cd6f0e4cfd8bf6f9f51ea0f
Ezekiel De Leon <ezekiel.d.e@ieee.org>
2019-06-28 21:55:09 -0700

    Added Spotlight for coin.

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import Page from '../Shared/Page';
-import PriceGrid from './priceGrid';
+import React from "react";
+import Page from "../Shared/Page";
+import PriceGrid from "./priceGrid";
+import CoinSpotlight from "./coinSpotlight";
+import styled from "styled-components";
+
+const ChardGrid = styled.div`
+  display: grid;
+  margin-top: 20px;
+  grid-gap: 15px;
+  grid-template-columns: 1fr 3fr;
+`;
 
 export default function() {
-	return (
+  return (
     <Page name="dashboard">
-      <PriceGrid></PriceGrid>
-		</Page>
-	);
+      <PriceGrid />
+      <ChardGrid>
+        <CoinSpotlight />
+        <div> Chart Goes Here!</div>
+      </ChardGrid>
+    </Page>
+  );
 }
